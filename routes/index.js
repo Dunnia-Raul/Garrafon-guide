505e34c1d95c70187e1fe64bf53910a30bfaddd2
Raúl Valenzuela <raulvalen7@hotmail.com>
2018-07-17 13:27:23 +0200

    CRUD Admin finished

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const ensureLogin = require("connect-ensure-login")
 const User = require("../models/User")
+const Drinks = require("../models/Drinks")
+const Places = require("../models/Places")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -9,11 +11,31 @@ router.get('/', (req, res, next) => {
 });
 
 router.get("/home", ensureLogin.ensureLoggedIn("/auth/login"), (req, res) => {
-  User.find({}).then( users => {
-    if(req.user.role.includes("admin")){
-      users.forEach(e => e.isAdmin = true);
-      res.render("list", {users});
+  User.find({}).then(users => {
+    if (req.user.role.includes("admin")) {
+      Places.find({}).then(places => {
+        Drinks.find({}).then(drinks => {
+          const data = {
+            users,
+            drinks,
+            places
+          }
+          res.render("list", data);
+        })
+      })
     } else res.render("home");
+
   })
- });
+});
+
+router.get("/delete/:id", (req, res) => {
+  User.findByIdAndRemove(req.params.id)
+    .then(() => {
+      Places.findByIdAndRemove(req.params.id)
+        .then(() => {
+          Drinks.findByIdAndRemove(req.params.id)
+            .then(() => res.redirect("/home"))
+        })
+    })
+})
 module.exports = router;
