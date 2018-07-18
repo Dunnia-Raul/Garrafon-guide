189b7afcac5566d03c1b593ac64dd1c8343d3a8c
Dunnia <dunnia.alvarez@gmail.com>
2018-07-18 20:10:53 +0200

    new comment and list commnent

--- routes/index.js
+++ routes/index.js
@@ -1,131 +1,123 @@
 const express = require('express');
 const router = express.Router();
-const {ensureLoggedIn, ensureLoggedOut} = require("connect-ensure-login")
-const User = require("../models/User")
+const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const User = require('../models/User');
 const { sendMail } = require('../mailing/sendMail');
-const Drinks = require("../models/Drinks")
-const Places = require("../models/Places")
-const Comments = require("../models/Comments")
+const Drinks = require('../models/Drinks');
+const Places = require('../models/Places');
+const Comments = require('../models/Comments');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+	res.render('index');
 });
 
-
-router.get("/home",ensureLoggedIn("/auth/login"), (req, res) => {
-  User.find({}).then(users => {
-    if (req.user.role.includes("admin")) {
-      Places.find({}).then(places => {
-        Drinks.find({}).then(drinks => {
-          const data = {
-            users,
-            drinks,
-            places
-          }
-          res.render("list", data);
-        })
-      })
-    } else {
-      Places.find({}).then(places => {
-        res.render("home", { places, placesAux: JSON.stringify(places) })
-      });
-    };
-
-  })
+router.get('/home', ensureLoggedIn('/auth/login'), (req, res) => {
+	User.find({}).then((users) => {
+		if (req.user.role.includes('admin')) {
+			Places.find({}).then((places) => {
+				Drinks.find({}).then((drinks) => {
+					const data = {
+						users,
+						drinks,
+						places
+					};
+					res.render('list', data);
+				});
+			});
+		} else {
+			Places.find({}).then((places) => {
+				res.render('home', { places, placesAux: JSON.stringify(places) });
+			});
+		}
+	});
 });
 
-router.get("/bars/add", (req, res, next) => {
-  res.render("add");
-})
+router.get('/bars/add', (req, res, next) => {
+	res.render('add');
+});
 
-router.post("/bars/add", (req, res, next) => {
-  const { name, zone, city, comments, capacity } = req.body;
-  const location = {
-    type = 'Point',
-    coordinates: [Number(req.body.lat), Number(req.body.lng)]
-  }
-  Place.findOne({
-    name
-  })
-    .then(place => {
-      if (place !== null) {
-        throw new Error("name Already exists");
-      }
-      const newPlace = new Places({ name, zone, city, comments, capacity, location })
-      return newPlace.save()
-    })
-    .then(place => {
-      console.log("añade")
-      res.redirect('/home')
-    })
-    .catch(err => {
-      console.log(err);
-      Place.find().then(places => {
-        res.render("home", { places: JSON.stringify(places) });
-
-      })
-    })
-  })
-  router.get("/delete/:id", (req, res) => {
-    User.findByIdAndRemove(req.params.id)
-      .then(() => {
-        Places.findByIdAndRemove(req.params.id)
-          .then(() => {
-            Drinks.findByIdAndRemove(req.params.id)
-              .then(() => res.redirect("/home"))
-          })
-      })
-  })
-
+router.post('/bars/add', (req, res, next) => {
+	const { name, zone, city, comments, capacity } = req.body;
+	const location = {
+		//type = 'Point',
+		coordinates: [ Number(req.body.lat), Number(req.body.lng) ]
+	};
+	Places.findOne({
+		name
+	})
+		.then((place) => {
+			if (place !== null) {
+				throw new Error('name Already exists');
+			}
+			const newPlace = new Places({ name, zone, city, comments, capacity, location });
+			return newPlace.save();
+		})
+		.then((place) => {
+			console.log('añade');
+			res.redirect('/home');
+		})
+		.catch((err) => {
+			console.log(err);
+			Place.find().then((places) => {
+				res.render('home', { places: JSON.stringify(places) });
+			});
+		});
+});
+router.get('/delete/:id', (req, res) => {
+	User.findByIdAndRemove(req.params.id).then(() => {
+		Places.findByIdAndRemove(req.params.id).then(() => {
+			Drinks.findByIdAndRemove(req.params.id).then(() => res.redirect('/home'));
+		});
+	});
+});
 
 /*****details Bar****/
 router.get('/bars/:id', (req, res, next) => {
-  Places.findById(req.params.id)
-    .then( (place) => {
-    Comments.find({place: req.params.id })
-    .populate("garrafon")
-    .populate("creator")
-    .then((comments)=>{
-      const data={
-        place,
-        comments
-      }
-      console.log(data.comments)
-      res.render('detailsBar', data); 
-    })
-    
-    })
-    .catch( (err) => {console.log(err)});
+	Places.findById(req.params.id)
+		.then((place) => {
+			Comments.find({ place: req.params.id }).populate('garrafon').populate('creator').then((comments) => {
+				const data = {
+					place,
+					comments
+				};
+				console.log(data.comments);
+				res.render('detailsBar', data);
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
-router.get("bars/:id/newComment", (req, res, next) => {
- Places.findById(req.params.id)
-    .then((place) => {
-      res.render('newComment', {place});
-    });
-  
+router.get('/comments/:id/newComment', (req, res, next) => {
+	Places.findById(req.params.id).then((place) => {
+    console.log(place)
+		Drinks.find().then((drinks) => {
+			const data = {
+				drinks,
+				place
+			};
+
+			res.render('newComment', data);
+		});
+	});
 });
 
-/*router.post("/bars/:id/newComment", (req, res, next) => {
-const {content}=req.body
-new Comment({content})
-.save()
-     .then((comment) => {
-       res.render('/newComment', {place});
-     });
-   
- });*/
-
-//  Drinks.find()
-//       .then((drinks)=> {
-//       const data = {
-//         drinks,
-//         place    
-//       }
-//       res.render('detailsBar', data);
- 
-
-
+router.post('/comments/:id/newComment', (req, res, next) => {
+  console.log('req.user')
+  console.log(req.user)
+  console.log('req.body.garrafon')
+  console.log(req.body.garrafon)
+  const data = {
+		comments: req.body.content,
+    garrafon: req.body.garrafon,
+    place: req.params.id,
+    creator: req.user._id
+	};
+	new Comments(data).save().then(() => {
+		res.redirect(`/bars/${req.params.id}`);
+	});
+});
 
 module.exports = router;
